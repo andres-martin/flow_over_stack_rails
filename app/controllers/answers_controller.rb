6d605c9de219551ce186f60ff9fa73cc154249d5
andres-martin <andresmartin266@gmail.com>
2019-09-01 20:02:05 -0500

    answers controller and questions-show view modified

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
 class AnswersController < ApplicationController
+ 
+  # def create
+  #   question = Question.find(params[:question_id])
+  #   if question.answers.create(answers_params)
+  #     flash[:info] = 'Answer created'
+  #     redirect_to question
+  #   else
+  #     render :question
+  #   end
+  # end
+
   def create
     question = Question.find(params[:question_id])
-    question.answers.create(answers_params)
-    flash[:info] = 'Answer created'
-    redirect_to question
+    answer = question.answers.create(answers_params)
+    if answer.save
+      flash[:info] = 'Answer created'
+      redirect_to question
+    else
+      flash[:alert] = "Answer can't be empty"
+      redirect_to question
+    end  
   end
-
   private
 
   def answers_params
